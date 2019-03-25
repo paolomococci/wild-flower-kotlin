f0ededb1c038e51ad5fd904fa82e39bc419264ed
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-03-25 15:37:57 +0100

    Update TypedInstrumentedTests.kt

--- app/src/androidTest/java/local/example/wildflower/TypedInstrumentedTests.kt
+++ app/src/androidTest/java/local/example/wildflower/TypedInstrumentedTests.kt
@@ -18,12 +18,86 @@
 
 package local.example.wildflower
 
-
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.clearText
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+
+import local.example.wildflower.R.id.main_edit_text
+import local.example.wildflower.R.id.message_text_from_reply
+import local.example.wildflower.R.id.reply_button
+import local.example.wildflower.R.id.reply_edit_text
+import local.example.wildflower.R.id.send_button
+import local.example.wildflower.R.id.text_message
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
 class TypedInstrumentedTests {
 
+    /* remember to switch on the screen on testing device */
+
+    private var toBeTypedMain: String? = null
+    private var toBeTypedReply: String? = null
+
+    @Rule
+    var mainActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Rule
+    var replyActivityTestRule = ActivityTestRule(ReplyActivity::class.java)
+
+    @Before
+    fun setUp() {
+        toBeTypedMain = "Welcome to Eden."
+        toBeTypedReply = "Welcome to my world!"
+        mainActivityTestRule.activity.supportFragmentManager.beginTransaction()
+    }
+
     @Test
-    fun sampleTest() {
+    fun clearedAndTypedTest() {
+        onView(withId(main_edit_text)).check(matches(isDisplayed()))
+        onView(withId(main_edit_text)).perform(
+            clearText(),
+            typeText("Welcome."), closeSoftKeyboard()
+        )
+    }
+
+    @Test
+    fun typedAndVerifyMessageTest() {
+        onView(withId(main_edit_text)).perform(
+            clearText(), typeText(toBeTypedMain!!),
+            closeSoftKeyboard()
+        )
+        onView(withId(send_button)).perform(click())
+        onView(withId(text_message)).check(matches(withText(toBeTypedMain)))
+    }
+
+    @Test
+    fun mainMessageAndReplyTest() {
+        onView(withId(main_edit_text)).perform(
+            clearText(), typeText(toBeTypedMain!!),
+            closeSoftKeyboard()
+        )
+        onView(withId(send_button)).perform(click())
+        onView(withId(text_message)).check(matches(withText(toBeTypedMain)))
+        onView(withId(reply_edit_text)).perform(
+            clearText(), typeText(toBeTypedReply!!),
+            closeSoftKeyboard()
+        )
+        onView(withId(reply_button)).perform(click())
+        onView(withId(message_text_from_reply)).check(matches(withText(toBeTypedReply)))
     }
 }
